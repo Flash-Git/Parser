de70a8836d9b65ffe544470c6da7851a5b1c809a
J Quinn <git@jaquinn.com>
2020-02-26 11:44:22 +0100

    Removed template code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,9 @@
 import React, { FC, useContext, useState } from "react";
 
-import { useMountEffect } from "utils/hooks";
-
-import AlertContext from "context/alert/AlertContext";
-import { AddAlert, ClearAlerts } from "context";
-
 const Home: FC = () => {
-  const alertContext = useContext(AlertContext);
-  const {
-    addAlert,
-    clearAlerts
-  }: { addAlert: AddAlert; clearAlerts: ClearAlerts } = alertContext;
-
-  const [counter, setCounter] = useState(1);
-
-  useMountEffect(() => {
-    addAlert("This is an alert!", "danger", 5000);
-  });
-
-  const onClick = () => {
-    addAlert(`Hello: ${counter}`, "light");
-    setCounter(counter + 1);
-  };
-
   return (
     <div className="container flex col center">
-      <h1>Hello World</h1>
-      <div className="row">
-        <button className="btn m" onClick={onClick}>
-          New Alert
-        </button>
-        <button className="btn m" onClick={clearAlerts}>
-          Clear Alerts
-        </button>
-      </div>
+      <h1>Transaction Parser</h1>
     </div>
   );
 };
