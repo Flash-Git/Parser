fdf9d99fc628ff73f4db03a835815c74c7671050
J Quinn <git@jaquinn.com>
2020-02-27 11:31:21 +0100

    Added NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+const NotFound: FC = () => (
+  <div className="container">
+    <h1>Not Found</h1>
+    <p className="lead">The page you are looking for does not exist.</p>
+  </div>
+);
+
+export default NotFound;
