5992629dc9c1cf8a2f3c7cd8d11413e54d0aca32
J Quinn <git@jaquinn.com>
2020-02-27 11:45:42 +0100

    Updated About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,9 +7,12 @@ interface Props {
 const About: FC<Props> = ({ version }) => (
   <div className="container">
     <h1>About</h1>
-    <h3>React based application for parsing transactions</h3>
-    <p>Create from Web3 Template</p>
-    <p>{`Version: ${version}`}</p>
+    <p className="my-1">React based application for parsing transactions.</p>
+    <p>Created from the Web3 Template.</p>
+    <p className="bg-dark p my-1">
+      <strong>Version:&nbsp;</strong>
+      {version}
+    </p>
   </div>
 );
 
