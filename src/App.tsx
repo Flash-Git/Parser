b248a50bc3130a4102915f958fc303ce6cab8928
J Quinn <git@jaquinn.com>
2020-02-27 11:32:12 +0100

    Implemented router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, { FC } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { faInfoCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import { faEthereum } from "@fortawesome/free-brands-svg-icons";
+import "./App.css";
 
 import Alerts from "components/layout/Alerts";
-
 import Home from "pages/Home";
+import About from "pages/About";
+import NotFound from "pages/NotFound";
 
 import AlertState from "context/alert/AlertState";
 import Web3State from "context/web3/Web3State";
-
-import "./App.css";
 
 library.add(faInfoCircle, faTimesCircle, faEthereum);
 
@@ -19,8 +20,20 @@ const version = "0.1.0";
 const App: FC = () => (
   <AlertState>
     <Web3State>
-      <Alerts />
-      <Home />
+      <Router>
+        <Alerts />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About version={version} />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </Web3State>
   </AlertState>
 );
