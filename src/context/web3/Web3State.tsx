97c9d1bde5b311c192f32205c9cd2c9ef36ed687
J Quinn <git@jaquinn.com>
2020-02-28 14:27:21 +0100

    Added Web3State type

--- src/context/web3/Web3State.tsx
+++ src/context/web3/Web3State.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useReducer } from "react";
 import { Provider } from "ethers/providers";
+import { Web3State } from "context";
 
 import Web3Context from "./Web3Context";
 import Web3Reducer from "./Web3Reducer";
@@ -7,7 +8,7 @@ import Web3Reducer from "./Web3Reducer";
 import { SET_PROVIDER } from "../types";
 
 const Web3State: FC = props => {
-  const initialState: any = { provider: null };
+  const initialState: Web3State = { provider: null };
 
   const [state, dispatch] = useReducer(Web3Reducer, initialState);
 
