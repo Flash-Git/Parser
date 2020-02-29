02f6e5c72570dde4f3b13e0a83e8620a3ade1687
J Quinn <git@jaquinn.com>
2020-02-29 16:06:49 +0100

    Removed unnecessary div

--- src/components/parsers/ParserSelector.tsx
+++ src/components/parsers/ParserSelector.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, Fragment } from "react";
 import { setType } from "parsers";
 import { PARSER_TYPES } from "components/layout/Parser";
 
@@ -6,16 +6,13 @@ interface Props {
   setType: setType;
 }
 
-const ParserSelector: FC<Props> = ({ setType }) => {
-  return (
-    <div>
-      <button className="btn mx" onClick={() => setType(PARSER_TYPES.txLookup)}>
-        Tx Lookup
-      </button>
-      <button className="btn mx">Two</button>
-      <button className="btn mx">Three</button>
-    </div>
-  );
-};
-
+const ParserSelector: FC<Props> = ({ setType }) => (
+  <Fragment>
+    <button className="btn mx" onClick={() => setType(PARSER_TYPES.txLookup)}>
+      Tx Lookup
+    </button>
+    <button className="btn mx">Two</button>
+    <button className="btn mx">Three</button>
+  </Fragment>
+);
 export default ParserSelector;
