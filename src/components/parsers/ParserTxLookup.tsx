9dda4f67f57fbbba4ff44cdc2c7df474cfd8b536
J Quinn <git@jaquinn.com>
2020-02-29 16:10:48 +0100

    Removed unused code

--- src/components/parsers/ParserTxLookup.tsx
+++ src/components/parsers/ParserTxLookup.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, FormEvent, useContext } from "react";
+import React, { FC, useState, FormEvent, useContext, Fragment } from "react";
 import { resetType } from "parsers";
 import { ethers, utils } from "ethers";
 import { TransactionResponse } from "ethers/providers";
@@ -23,7 +23,6 @@ const ParserTxLookup: FC<Props> = ({ resetType }) => {
   // Input
   const [address, setAddress] = useState("");
   const [receivingAddresses, setReceivingAddresses] = useState("");
-  // const [topics, setTopics] = useState("transfer(address,uint256)");
   const [startBlock, setStartBlock] = useState("9500000"); // TODO accept date
 
   // Response
@@ -60,87 +59,72 @@ const ParserTxLookup: FC<Props> = ({ resetType }) => {
     console.log(filteredTxs);
   };
 
-  // const getEvents = async () => {
-  //   // const topic = ethers.utils.id("transfer(address,uint256)");
-  //   // const topic = ethers.utils.id("Created(bytes32)");
-
-  //   const parsedTopics = topics
-  //     .replace(/\s/g, "")
-  //     .split(";")
-  //     .map(topic => ethers.utils.id(topic));
-  //   console.log(topics.replace(/\s/g, "").split(";"));
-
-  //   const filter = {
-  //     fromBlock: parseInt(startBlock),
-  //     toBlock: "latest",
-  //     address,
-  //     topics: parsedTopics
-  //   };
-  //   const logs = await etherscanProvider.getLogs(filter);
-  //   setLogs(logs);
-  //   console.log(logs);
-  // };
-
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     getTransactions();
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit} className="flex col grow">
-        <div className="flex col center grow">
-          <div className="px">Address:</div>
-          <input
-            type="text"
-            value={address}
-            onChange={e => setAddress(e.target.value)}
-          />
-          <div className="px">Spending Addresses:</div>
-          <input
-            type="text"
-            value={receivingAddresses}
-            onChange={e => setReceivingAddresses(e.target.value)}
-          />
-          {/* <div className="px">Topics:</div>
-          <input
-            type="text"
-            value={topics}
-            onChange={e => setTopics(e.target.value)}
-          /> */}
-          <div className="px">Starting Block:</div>
-          <input
-            type="text"
-            value={startBlock}
-            onChange={e => setStartBlock(e.target.value)}
-          />
-        </div>
-        <div className="center text-center">
-          <button className="btn mx" type="submit">
+    <Fragment>
+      <form
+        className="col address m mx-2 px-1"
+        style={{ display: "inline-flex" }}
+        onSubmit={onSubmit}
+      >
+        <div className="px">Address:</div>
+        <input
+          type="text"
+          value={address}
+          onChange={e => setAddress(e.target.value)}
+        />
+        <div className="px">Spending Addresses:</div>
+        <input
+          type="text"
+          value={receivingAddresses}
+          onChange={e => setReceivingAddresses(e.target.value)}
+        />
+        <div className="px">Starting Block:</div>
+        <input
+          type="text"
+          value={startBlock}
+          onChange={e => setStartBlock(e.target.value)}
+        />
+        <div className="center text-center no-wrap">
+          <button className="btn m" type="submit">
             Lookup
           </button>
-          <button className="btn mx" onClick={() => resetType()}>
+          <button className="btn m" onClick={() => resetType()}>
             Reset Parser
           </button>
         </div>
       </form>
-      <div className="m-2" style={{ width: "100%" }}>
+      <div
+        className="m-2 px-1"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "0.5fr 0.5fr 7fr 5fr",
+          gap: "1rem",
+          textAlign: "left"
+        }}
+      >
         {transactions.map((tx, index: number) => (
-          <div key={index} className="grid-3">
-            {zeroPad(index, 3)}
-            {" - Tx Hash: "}
-            <a href={`https://etherscan.io/tx/${tx.hash}`}>{`${tx.hash.slice(
-              0,
-              5
-            )}...${tx.hash.slice(-5)}`}</a>
-            {`Value: ${utils.formatUnits(tx.value, "ether").substr(0, 8)}`}
-          </div>
+          <Fragment key={index}>
+            <div>{zeroPad(index, 3)}</div>
+            <div>{` - `}</div>
+            <div>
+              {"Tx Hash: "}
+              <a href={`https://etherscan.io/tx/${tx.hash}`}>{`${tx.hash.slice(
+                0,
+                7
+              )}...${tx.hash.slice(-6)}`}</a>
+            </div>
+            <div>
+              {`Value: ${utils.formatUnits(tx.value, "ether").substr(0, 8)}`}
+            </div>
+          </Fragment>
         ))}
-        {/* {logs.map((log, index: number) => (
-          <div key={index} style={{ width: "100%" }}></div>
-        ))} */}
       </div>
-    </div>
+    </Fragment>
   );
 };
 
