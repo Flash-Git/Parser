e2c9c1e382d7387a1a32f0a12ba19dea2676a910
J Quinn <git@jaquinn.com>
2020-03-01 13:16:33 +0100

    Added interface for fixedTxResponse

--- src/components/parsers/index.d.ts
+++ src/components/parsers/index.d.ts
@@ -1,7 +1,15 @@
 declare module "parsers" {
+  import { TransactionResponse } from "ethers/providers";
   import { PARSER_TYPES } from "components/layout/Parser";
 
   export type type = PARSER_TYPES;
   export type setType = (type: PARSER_TYPES) => void;
   export type resetType = () => void;
+
+  /*
+   * Ethers
+   */
+  export interface FixedTransactionResponse extends TransactionResponse {
+    creates?: string;
+  }
 }
