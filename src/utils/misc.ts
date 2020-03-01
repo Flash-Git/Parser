ce32399056cb059852ce54fe451e67444d2ab705
J Quinn <git@jaquinn.com>
2020-03-01 17:55:29 +0100

    Created utility methods isAddress and isENS

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,2 +1,23 @@
+import { utils } from "ethers";
+import { Provider } from "ethers/providers";
+
 export const zeroPad = (num: any, places: number) =>
   String(num).padStart(places, "0");
+
+export const isAddress = (address: string) => {
+  try {
+    utils.getAddress(address);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
+export const isENS = async (provider: Provider, name: string) => {
+  try {
+    if ((await provider.resolveName(name)) === null) throw new Error();
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
