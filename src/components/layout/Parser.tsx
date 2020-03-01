26c095baca360b316f69e31965eb5926376a5179
J Quinn <git@jaquinn.com>
2020-03-01 13:15:46 +0100

    Moved etherscan provider into parser

--- src/components/layout/Parser.tsx
+++ src/components/layout/Parser.tsx
@@ -1,14 +1,30 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useContext } from "react";
+import { ethers } from "ethers";
+
 import Selector from "components/parsers/ParserSelector";
 import TxLookup from "components/parsers/ParserTxLookup";
+import EventLookup from "components/parsers/ParserEventLookup";
+import { useMountEffect } from "utils/hooks";
+
+import Web3Context from "context/web3/Web3Context";
+import { Web3Context as IWeb3Context } from "context";
 
 export enum PARSER_TYPES {
   undefined,
-  txLookup
+  txLookup,
+  eventLookup
 }
 
 const Parser: FC = () => {
+  const web3Context: IWeb3Context = useContext(Web3Context);
+  const { etherscanProvider, setEtherscanProvider } = web3Context;
   const [type, setType] = useState(PARSER_TYPES.undefined);
+
+  const updateEtherscanProvider = () => {
+    setEtherscanProvider(new ethers.providers.EtherscanProvider());
+  };
+
+  useMountEffect(() => updateEtherscanProvider());
 
   const resetType = () => {
     setType(PARSER_TYPES.undefined);
@@ -18,7 +34,16 @@ const Parser: FC = () => {
     case PARSER_TYPES.undefined:
       return <Selector setType={setType} />;
     case PARSER_TYPES.txLookup:
-      return <TxLookup resetType={resetType} />;
+      return (
+        <TxLookup resetType={resetType} etherscanProvider={etherscanProvider} />
+      );
+    case PARSER_TYPES.eventLookup:
+      return (
+        <EventLookup
+          resetType={resetType}
+          etherscanProvider={etherscanProvider}
+        />
+      );
     default:
       throw new Error(`Unhandled parser type: ${type}`);
   }
