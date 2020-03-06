07eccbe9956e68b12b24766e178b6b44a706ba5b
J Quinn <git@jaquinn.com>
2020-03-06 20:33:02 +0100

    Formatting

--- src/components/layout/Spinner.tsx
+++ src/components/layout/Spinner.tsx
@@ -1,15 +1,13 @@
-import React, { Fragment } from "react";
+import React, { FC } from "react";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const spinner = require("res/spinner.gif");
 
-const Spinner: React.FC = () => (
-  <Fragment>
-    <img
-      src={spinner.default}
-      alt="Loading..."
-      style={{ width: "200px", margin: "auto", display: "block" }}
-    />
-  </Fragment>
+const Spinner: FC = () => (
+  <img
+    src={spinner.default}
+    alt="Loading..."
+    style={{ width: "200px", margin: "auto", display: "block" }}
+  />
 );
 
 export default Spinner;
